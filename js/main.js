5edd4a579c399a5204a0913b2ded6524bbae3e9c
Haresh <hefgames50@gmail.com>
2023-05-27 18:32:58 -0400

    everything updated

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
     const showClass = "show";
   
     $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
+        if (this.matchMedia("(min-width: 1067px)").matches) {
             $dropdown.hover(
             function() {
                 const $this = $(this);
